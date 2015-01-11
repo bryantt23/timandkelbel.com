62e80c92b7bb91851d7a307684daedc5fd45c535
Tim Smith <smith@ttimsmith.com>
2015-01-11 14:39:28 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'bourbon'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'foundation-rails'
+gem 'haml'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.0.8'
+gem 'sass-rails', '~> 4.0.2'
+gem 'thin'
+gem 'turbolinks'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'spring', group: :development
+gem 'uglifier', '>= 1.3.0'
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimandkelbelCom::Application.config.secret_key_base = 'dab4e273c7fce44848d7ddf07216ac0756cb37c8fc7cb1a129dc0e084df572e6e3160c8443f416514994f93ac002140545cac23c9f89d2ff36ceb1eea96e302d'
