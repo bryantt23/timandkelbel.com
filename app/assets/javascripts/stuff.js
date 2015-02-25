b5ce98bbc661df88872a9c44e9797997add8e543
Tim Smith <smith@ttimsmith.com>
2015-02-25 00:30:44 -0600

    Can't get this stupid slider to work

--- app/assets/javascripts/stuff.js
+++ app/assets/javascripts/stuff.js
@@ -1,3 +1,6 @@
+smoothScroll.init({
+  offset: 71
+});
 
 $(".toggleTopRSVP").on("click", function() {
   $("body").addClass("rsvpShow");
@@ -5,43 +8,44 @@ $(".toggleTopRSVP").on("click", function() {
 
 $(".removeTopRSVP").on("click", function() {
   $("body").removeClass("rsvpShow");
-})
+});
+
 
 $(document).ready(function($, window){
 
-    var $w = $(window);
-    var $body = $('body');
-    var tk = {
-        setup : {
-            test : function() {
-                return true;
-            },
-            run : function() {
+  var $w = $(window);
+  var $body = $('body');
+  var tk = {
+      setup : {
+          test : function() {
+              return true;
+          },
+          run : function() {
 
-            }
-        },
-        navScroll : {
-            test : function() {
-                return true;
-            },
-            run : function() {
-                var $header = $('.site__header');
-                $w.on('scroll', function(e){
-                    if ($w.scrollTop() > 50){
-                        $body.addClass('scrolled');
-                    } else {
-                        $body.removeClass('scrolled');
-                    }
-                });
-            }
-        }
-    };
+          }
+      },
+      navScroll : {
+          test : function() {
+              return true;
+          },
+          run : function() {
+              var $header = $('.site__header');
+              $w.on('scroll', function(e){
+                  if ($w.scrollTop() > 50){
+                      $body.addClass('scrolled');
+                  } else {
+                      $body.removeClass('scrolled');
+                  }
+              });
+          }
+      }
+  };
 
-    for (var key in tk){
-        if (tk[key].test()){
-            console.log(key);
-            tk[key].run();
-        }
-    }
+  for (var key in tk){
+      if (tk[key].test()){
+          console.log(key);
+          tk[key].run();
+      }
+  }
 
 }(jQuery, window));
